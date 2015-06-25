b2f6e086c88e49027bf9d3324415031d373e48d5
william <mkwmms@users.noreply.github.com>
2015-06-25 14:21:27 -0600

    Update osx-aliases.plugin.zsh

--- osx-aliases.plugin.zsh
+++ osx-aliases.plugin.zsh
@@ -1,98 +1,102 @@
-# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-function update() {
+#!/usr/bin/env zsh
 
-    if [[ "$1" == "-osx" ]]; then
-         Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+if [[ "$OSTYPE" =~ ^(darwin)+ ]]; then
+  # Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+  function update() {
+  
+      if [[ "$1" == "-osx" ]]; then
+        # Keep-alive: update existing `sudo` time stamp until `update` has finished
         sudo -v && while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
         echo "\033[0;34mUpgrading OSX Packages...\033[0m"
         sudo softwareupdate -i -a
-    fi
-
-    echo "\033[0;34mUpgrading Homebrew...\033[0m"
-    brew update; brew upgrade -all; brew cleanup; brew cask cleanup;
-    echo "\033[0;34mUpgrading npm...\033[0m"
-    npm install npm -g; npm update -g;
-    echo "\033[0;34mUpgrading gem...\033[0m"
-    sudo gem update --system; sudo gem update
-    echo "\033[0;34mUpgrading pip...\033[0m"
-    pip install --upgrade pip
-    pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U # upgrade outdated pip packages...
-}
-
-# IP addresses
-alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
-
-# Flush Directory Service cache
-alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
-
-# Clean up LaunchServices to remove duplicates in the “Open With” menu
-alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
-
-# View HTTP traffic
-alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
-alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
-
-# Canonical hex dump; some systems have this symlinked
-command -v hd > /dev/null || alias hd="hexdump -C"
-
-# OS X has no `md5sum`, so use `md5` as a fallback
-command -v md5sum > /dev/null || alias md5sum="md5"
-
-# OS X has no `sha1sum`, so use `shasum` as a fallback
-command -v sha1sum > /dev/null || alias sha1sum="shasum"
-
-# JavaScriptCore REPL
-jscbin="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc";
-[ -e "${jscbin}" ] && alias jsc="${jscbin}";
-unset jscbin;
-
-# Trim new lines and copy to clipboard
-alias c="tr -d '\n' | pbcopy"
-
-# Empty the Trash on all mounted volumes and the main HDD
-# Also, clear Apple’s System Logs to improve shell startup speed
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
-
-# Show/hide hidden files in Finder
-alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
-alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
-
-# Hide/show all desktop icons (useful when presenting)
-alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
-alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
-
-# URL-encode strings
-alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
-
-# Merge PDF files
-# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
-alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
-
-# PlistBuddy alias, because sometimes `defaults` just doesn’t cut it
-alias plistbuddy="/usr/libexec/PlistBuddy"
-
-# Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
-# (useful when executing time-consuming commands)
-alias badge="tput bel"
-
-# Intuitive map function
-# For example, to list all directories that contain a certain file:
-# find . -name .gitattributes | map dirname
-alias map="xargs -n1"
-
-# One of @janmoesen’s ProTip™s
-for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
-  alias "$method"="lwp-request -m '$method'"
-done
-
-# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
-alias stfu="osascript -e 'set volume output muted true'"
-alias pumpitup="osascript -e 'set volume 7'"
-
-# Diff
-alias gdiff='git diff --no-index --color-words '
-alias diff2='diff -y --suppress-common-lines '
-
-# turn ethernet on/off
-alias ethoff="sudo networksetup setnetworkserviceenabled 'Ethernet 1' off"
-alias ethon="sudo networksetup setnetworkserviceenabled 'Ethernet 1' on"
+      fi
+  
+      echo "\033[0;34mUpgrading Homebrew...\033[0m"
+      brew update; brew upgrade -all; brew cleanup; brew cask cleanup;
+      echo "\033[0;34mUpgrading npm...\033[0m"
+      npm install npm -g; npm update -g;
+      echo "\033[0;34mUpgrading gem...\033[0m"
+      sudo gem update --system; sudo gem update
+      echo "\033[0;34mUpgrading pip...\033[0m"
+      pip install --upgrade pip
+      pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U # upgrade outdated pip packages...
+  }
+  
+  # IP addresses
+  alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+  
+  # Flush Directory Service cache
+  alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+  
+  # Clean up LaunchServices to remove duplicates in the “Open With” menu
+  alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+  
+  # View HTTP traffic
+  alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+  alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+  
+  # Canonical hex dump; some systems have this symlinked
+  command -v hd > /dev/null || alias hd="hexdump -C"
+  
+  # OS X has no `md5sum`, so use `md5` as a fallback
+  command -v md5sum > /dev/null || alias md5sum="md5"
+  
+  # OS X has no `sha1sum`, so use `shasum` as a fallback
+  command -v sha1sum > /dev/null || alias sha1sum="shasum"
+  
+  # JavaScriptCore REPL
+  jscbin="/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc";
+  [ -e "${jscbin}" ] && alias jsc="${jscbin}";
+  unset jscbin;
+  
+  # Trim new lines and copy to clipboard
+  alias c="tr -d '\n' | pbcopy"
+  
+  # Empty the Trash on all mounted volumes and the main HDD
+  # Also, clear Apple’s System Logs to improve shell startup speed
+  alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+  
+  # Show/hide hidden files in Finder
+  alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+  alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+  
+  # Hide/show all desktop icons (useful when presenting)
+  alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+  alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+  
+  # URL-encode strings
+  alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1]);"'
+  
+  # Merge PDF files
+  # Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
+  alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
+  
+  # PlistBuddy alias, because sometimes `defaults` just doesn’t cut it
+  alias plistbuddy="/usr/libexec/PlistBuddy"
+  
+  # Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
+  # (useful when executing time-consuming commands)
+  alias badge="tput bel"
+  
+  # Intuitive map function
+  # For example, to list all directories that contain a certain file:
+  # find . -name .gitattributes | map dirname
+  alias map="xargs -n1"
+  
+  # One of @janmoesen’s ProTip™s
+  for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+    alias "$method"="lwp-request -m '$method'"
+  done
+  
+  # Stuff I never really use but cannot delete either because of http://xkcd.com/530/
+  alias stfu="osascript -e 'set volume output muted true'"
+  alias pumpitup="osascript -e 'set volume 7'"
+  
+  # Diff
+  alias gdiff='git diff --no-index --color-words '
+  alias diff2='diff -y --suppress-common-lines '
+  
+  # turn ethernet on/off
+  alias ethoff="sudo networksetup setnetworkserviceenabled 'Ethernet 1' off"
+  alias ethon="sudo networksetup setnetworkserviceenabled 'Ethernet 1' on"
+fi
